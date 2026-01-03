52fe3734f8780c0cd6dd62c01ea5514ae1408c09
bka1605 <briankristanto.alim@gmail.com>
2026-01-03 23:24:34 +0700

    biar chrome ga meledak

--- app.py
+++ app.py
@@ -75,9 +75,13 @@ def preprocess_hybrid(text):
 def run_scraper(penulis_input, limit_data):
     # SETUP CHROME OPTIONS 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless=new")
+    # chrome_options.add_argument("--headless=new")
+    
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--window-size=1920,1080")
 
     # menggunakan useragent asli agar tidak diblokir google
